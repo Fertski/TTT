fcbf053f56431bf8da16b7d79fe6edf11c71512a
Fertski <74899200+Fertski@users.noreply.github.com>
2020-12-02 10:51:16 +0000

    Add files via upload

---
+++ tictactoe.1.3.js
@@ -0,0 +1,71 @@
+const canvas = document.getElementById("gamecanvas");
+const canvasLeft = canvas.offsetLeft + canvas.clientLeft;
+const canvasTop = canvas.offsetTop + canvas.clientTop;
+const ctx = canvas.getContext("2d");
+var boardSize = 3;
+
+const cellSize = (document.getElementById("gamecanvas").width)/boardSize;
+const emptyCell = {colour: "White", top:cellSize, left:cellSize, full: false};
+const playerCell = {colour: "Red", top:cellSize, left:cellSize, full: true};
+const aiCell = {colour: "Blue", top:cellSize, left:cellSize, full: true};
+
+
+var board = [];
+
+//Create board array full of empty cells
+for (r = 0; r < boardSize; r++){
+	board[r] = [];
+	
+	for (c = 0; c < boardSize; c++){
+		board[r][c] = emptyCell;
+	}
+}
+
+//Click detection 
+canvas.addEventListener("click", function(event){
+	var x = event.pageX - canvasLeft;
+	var y = event.pageY - canvasTop;
+	var cellX = Math.floor(x/cellSize);
+	var cellY = Math.floor(y/cellSize);
+	//Checks if player has clicked within board confines.
+	if (cellX < boardSize && cellY < boardSize && turnState == true && board[cellY][cellX].full == false){
+			drawCell(cellX*cellSize, cellY*cellSize, "Red");
+			board[cellY][cellX] = playerCell;
+			//board[cellY][cellX].colour = "Red";
+			turnState = !turnState;
+			drawBoard();
+			aiTurn();
+	}	
+});
+
+
+//Fills Cell
+function drawCell(x,y,colour){
+
+		ctx.fillStyle = colour;
+		ctx.fillRect(x,y,cellSize,cellSize);
+		
+		ctx.strokeStyle = "Black";
+		ctx.strokeRect(x,y,cellSize,cellSize);
+};
+	
+function drawBoard(){
+
+	for (r = 0; r < boardSize; r++){
+		for (c = 0; c < boardSize; c++){
+			//These bits make sure each cell individually reports back to EventListener
+			board[r].forEach(function(element) {
+				drawCell(element.left*c, element.top*r, board[r][c].colour);
+
+			});
+		}
+	}
+}
+
+drawBoard();
+var turnState = true;
+
+
+
+
+
--- tictactoeAI.js
+++ tictactoeAI.js
@@ -1,50 +1,163 @@
-var move = 0;
-var ownColour = "Blue";
-
-//function drawCell(x,y,width,height,colour){
-
-
-
-
+const ownColour = "Blue";
+const enemyColour = "Red";
+//function drawCell(x,y,width,height,colour
 function aiTurn(){
+	if (turnState == true){
+		return;
+	}
 
-	for (r = 0; r < boardSize - r ; r++){
-		for (c = 0; c < boardSize - c; c++){
-			if (board[r][c].colour == ownColour && board[2-r][2-c].colour == ownColour){
-				if (board[Math.abs(1-r)][Math.abs(1-c)].colour == emptyCell.colour){
-					move = canWin;
-					makeMove(Math.abs(1-c),Math.abs(1-r),move);
-					break;
+	const moveList = [
+		
+		[function canWin(r,c) {
+			for (x = -1; x < 1; x++){
+				for(y = -1; y < 1 ; y++){
+					//check center wins
+					if (board[1][1].colour == ownColour && board[y+1][x+1].colour == ownColour && board[1-y][1-x].colour == emptyCell.colour){
+						makeMove(1-x, 1-y);
+						return;
+					}
+				}
+			}
+			//Check edges
+			if (board[0][0].colour == ownColour && board[0][2].colour == ownColour && board[0][1].colour == emptyCell.colour){
+				makeMove(1,0);
+				return;
+			}
+			if (board[0][0].colour == ownColour && board[2][0].colour == ownColour && board[1][0].colour == emptyCell.colour){
+				makeMove(0,1);
+				return;
+			}	
+			if (board[2][2].colour == ownColour && board[0][2].colour == ownColour && board[0][1].colour == emptyCell.colour){
+				makeMove(1,0);
+				return;
+			}
+			if (board[2][2].colour == ownColour && board[2][0].colour == ownColour && board[1][0].colour == emptyCell.colour){
+				makeMove(0,1);
+				return;
+			}	
+		}],
+		
+		[function blockEnemyWin(r,c) {
+			for (x = -1; x < 1; x++){
+				for(y = -1; y < 1 ; y++){
+					//check center wins
+					if (board[1][1].colour == enemyColour && board[y+1][x+1].colour == enemyColour && board[1-y][1-x].colour == emptyCell.colour){
+						makeMove(1-x, 1-y);
+						return;
+					}
+				}
+			}
+			//Check edges
+			if (board[0][0].colour == enemyColour && board[0][2].colour == enemyColour && board[0][1].colour == emptyCell.colour){
+				makeMove(1,0);
+				return;
+			}
+			if (board[0][0].colour == enemyColour && board[2][0].colour == enemyColour && board[1][0].colour == emptyCell.colour){
+				makeMove(0,1);
+				return;
+			}	
+			if (board[2][2].colour == enemyColour && board[0][2].colour == enemyColour && board[0][1].colour == emptyCell.colour){
+				makeMove(1,0);
+				return;
+			}
+			if (board[2][2].colour == enemyColour && board[2][0].colour == enemyColour && board[1][0].colour == emptyCell.colour){
+				makeMove(0,1);
+				return;
+			}
+		}],
+		
+		[function makeFork(r,c) {
+			// X fork
+			if (board[0][0].colour == ownColour && board[0][2].colour == ownColour && board[1][1].colour == emptyCell.colour ){
+				if (board[2][0].colour == emptyCell.colour && board[2][2].colour == emptyCell.colour){
+					makeMove(1,1);
+					return;
+				}
+				if (board[2][0].colour == emptyCell.colour && board[1][0].colour == emptyCell.colour){
+					makeMove(0,2);
+					return;
+				}
+				if (board[2][2].colour == emptyCell.colour && board[1][2].colour == emptyCell.colour){
+					makeMove(2,2);
+					return;
 				}
 			}
 		}
-	}
-}
 
 
 
-function makeMove(x,y,move){
-	switch(x,y,move){
-	
-		case canWin:
-			drawCell(x,y,cellSize, cellSize, ownColour);
-			turnState = !turnState;
+
+
+
+
+			if (board[0][0].colour == enemyColour && board[2][0].colour == enemyColour && board[1][0].colour == emptyCell.colour){
+				makeMove(0,1);
+				return;
+			}	
+			if (board[2][2].colour == enemyColour && board[0][2].colour == enemyColour && board[0][1].colour == emptyCell.colour){
+				makeMove(1,0);
+				return;
+			}
+			if (board[2][2].colour == enemyColour && board[2][0].colour == enemyColour && board[1][0].colour == emptyCell.colour){
+				makeMove(0,1);
+				return;
+			}
+		}],
+		[function blockFork(r,c) { 
+			
+		}],
+		[function takeCenter(r,c) { }],
+		[function blockCorner(r,c) { }],
+		[function takeCorner(r,c) { }],
+		[function takeSide(r,c) { 
+			for (x = -1; x < 1; x++){
+				for(y = -1; y < 1 ; y++){
+					//check center wins
+					if (board[y+1][x+1].colour == emptyCell.colour){
+						makeMove(x+1,y+1);
+						return;
+					}
+				}
+			}
+		}]
+
+	];
+
+
+
+	//function boardChecker(currentMove){
+
+	//	for (r = 0; r < boardSize ; r++){
+	//		for (c = 0; c < boardSize; c++){
+	//			if (turnState == true){
+	//				break;
+	//			}
+			//currentMove(r,c);
+	//		moveList[currentMove][0](r,c);
+	//		}
+	//	}
+	//}
+	for ( i = 0; i < moveList.length; i++){
+		moveList[i][0]();
+		if ( turnState == true ){
 			break;
-		case blockEnemyWin:
-		
-		case makeFork:
-		
-		case blockFork:
-		
-		case takeCenter:
-		
-		case blockCorner:
-		
-		case takeCorker:
-		
-		case takeSide:
-		
-
+		}
 	}
-	
+
+
+
+	function makeMove(x,y){
+		drawCell(x*cellSize, y*cellSize, ownColour);
+		board[y][x] = aiCell;
+
+		turnState = !turnState;
+		drawBoard();
+		//aiTurn();
+	}
 }
+
+
+
+
+
+
